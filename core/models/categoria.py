590177fa4e7e84e7fc50da7c0ea3a0fb6deb62c3
EvelinROdriguess <evelinrodrigues@ifc.com>
2024-03-04 11:54:28 -0300

    def

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -3,3 +3,8 @@ from django.db import models
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.descricao
+        return f"{self.descricao}({self.id})"
+
+
