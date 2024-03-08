9f618aed59c7eea4ff1ebaafa202679f6a879dc6
EvelinRodriguess <evelinrodrigues.ifc@gmail.com>
2024-03-08 11:38:19 -0300

    criação api rest

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -4,7 +4,7 @@ class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descricao
+        # return self.descricao
         return f"{self.descricao}({self.id})"
 
 
